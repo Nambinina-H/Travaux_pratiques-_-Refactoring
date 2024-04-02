1460464ed3025f76bc872851b6ad4bc1e4b567d9
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2024-04-02 06:57:27 +0000

    5.Création de la classe Humain et faire hériter les classes Homme et Femme de la classe Humain 6.Déplacement  des attributs communs de Homme et Femme vers Humain

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,13 +1,7 @@
 package iut.bad;
 
-public class Homme {
-	   private String nom;
-	   private String prenom;
-	   private int age;
-
+public class Homme extends Humain {
 	   public Homme(String nom, String prenom, int age) {
-	     this.nom = nom;
-	     this.prenom = prenom;
-	     this.age = age;
+	     super(nom, prenom, age);
 	   }
 	}
