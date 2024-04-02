ee35ace0c0426e5f1d214e1fefdea4a5693470c4
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2024-04-02 07:16:31 +0000

    13.Ajout d' une méthode main() à la classe Femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -3,5 +3,11 @@ package iut.bad;
 public class Femme extends Humain {
 	   public Femme(String nom, String prenom, int age) {
 	     super(nom, prenom, age);
-	   }
+	   	}
+	   
+	   public static void main(String[] args) {
+		     Homme homme = new Homme("Homme", "Un", 25);
+		     Femme femme = new Femme("Femme", "Une", 24);
+		     femme.ami(homme);
+		   }
 	}
